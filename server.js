3b4661ba2483fa221c233c27601c6f032f472973
Aaron Neslin <aneslin125@gmail.com>
2022-02-02 21:06:54 -0500

    animal routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+app.use(express.static('public'))
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = []
@@ -72,9 +72,21 @@ function validateAnimal(animal){
 
 //routes
 
-app.get('/', (req,res =>{
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'./public/index.html'));
-}));
+});
+
+app.get('/animals', (req,res)=>{
+res.sendFile(path.join(__dirname,'./public/animals.html'));
+});
+
+app.get('/zookeepers', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.get('/api/animals/:id', (req,res)=> {
     const result = findById(req.params.id, animals);
